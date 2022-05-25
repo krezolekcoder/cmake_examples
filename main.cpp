9b7105dac98d57f2b80271bd18aea8fb9eb2681f
krezolekcoder <kkrezole@gmail.com>
2022-05-25 18:51:00 +0200

    Cmake example build

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream> 
+#include <adder.h>
+
+int main(void)
+{
+    std::cout << "Hello world !" << std::endl; 
+
+    std::cout << add(23.4, 23.0) << std::endl; 
+
+    return 0; 
+}
