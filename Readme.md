3489599fc6b99b5ee7b1f1e5de7d1754246c54a1
krezolekcoder <kkrezole@gmail.com>
2022-05-25 20:31:42 +0200

    Added glfw library building to CMakeExample project with build scripts

---
+++ Readme.md
@@ -0,0 +1,7 @@
+configure and build with cmake 
+
+
+linux command line scripts to configure build and run : 
+./configure.sh
+./build.sh
+./run.sh
--- main.cpp
+++ main.cpp
@@ -1,11 +1,49 @@
 #include <iostream> 
 #include <adder.h>
-
+#include <GLFW/glfw3.h>
 int main(void)
 {
-    std::cout << "Hello world !" << std::endl; 
+    std::cout << "Hello world mooorda !" << std::endl; 
 
     std::cout << add(23.4, 23.0) << std::endl; 
+
+    GLFWwindow* window;
+    int width, height;
+
+    if( !glfwInit() )
+    {
+        fprintf( stderr, "Failed to initialize GLFW\n" );
+        exit( EXIT_FAILURE );
+    }
+
+    window = glfwCreateWindow( 300, 300, "Gears", NULL, NULL );
+    if (!window)
+    {
+        fprintf( stderr, "Failed to open GLFW window\n" );
+        glfwTerminate();
+        exit( EXIT_FAILURE );
+    }
+
+    // Main loop
+    while( !glfwWindowShouldClose(window) )
+    {
+        // // Draw gears
+        // draw();
+
+        // // Update animation
+        // animate();
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // // Exit program
+    // exit( EXIT_SUCCESS );
+
 
     return 0; 
 }
